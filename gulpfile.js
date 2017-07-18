68fe5ec58737ee8d3f5abf00e4a098616a24a16a
Maria Chapman <thunderstarry@gmail.com>
2017-07-18 20:48:53 +0100

    Add a gulp build with Sass and autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const ghPages = require('gulp-gh-pages');
+const sass = require('gulp-sass');
+
+const BUILD_FOLDER = path.join(__dirname, 'build');
+
+
+gulp.task('html', () => {
+  return gulp.src('index.html')
+    .pipe(gulp.dest(BUILD_FOLDER));
+});
+
+gulp.task('js', () => {
+  return gulp.src('script.js')
+    .pipe(gulp.dest(BUILD_FOLDER));
+})
+
+gulp.task('sass', () => {
+  return gulp.src('styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(BUILD_FOLDER));
+});
+
+gulp.task('deploy', ['html', 'js', 'sass'], () => {
+  return gulp.src(path.join(BUILD_FOLDER, '**', '*'))
+    .pipe(ghPages());
+});
