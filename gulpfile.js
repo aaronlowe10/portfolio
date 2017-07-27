5a3c17fffcc4d3ef5673dce0b10ae46a3eeaa604
Maria Chapman <thunderstarry@gmail.com>
2017-07-27 21:00:57 +0100

    Move styles into their own folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const ghPages = require('gulp-gh-pages');
 const sass = require('gulp-sass');
 
 const BUILD_FOLDER = path.join(__dirname, 'build');
+const MAIN_STYLE_FILE = path.join(__dirname, 'styles', 'styles.scss');
 
 
 gulp.task('html', () => {
@@ -24,21 +25,21 @@ gulp.task('images', () => {
 });
 
 gulp.task('sass', () => {
-  return gulp.src('styles.scss')
+  return gulp.src(MAIN_STYLE_FILE)
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest(BUILD_FOLDER));
 });
 
 gulp.task('sass-dev', () => {
-  return gulp.src('styles.scss')
+  return gulp.src(MAIN_STYLE_FILE)
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest(__dirname));
 });
 
 gulp.task('dev', () => {
-  gulp.watch('styles.scss', ['sass-dev']);
+  gulp.watch(path.join('styles', '*'), ['sass-dev']);
 });
 
 gulp.task('deploy', ['html', 'js', 'images', 'sass'], () => {
